e3f7fa949be3031ef9b7c66d174273bf78fab361
huyanwei <hywchina@126.com>
2025-08-11 21:44:54 +0800

    add requirements.txt

--- start.sh
+++ start.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-# 安装依赖
-# pip install graphrag future
+# # 安装依赖 方式1
+# pip install -e "libs/kotaemon[all]"
+# pip install -e "libs/ktem"
+# # pip uninstall hnswlib chroma-hnswlib && pip install chroma-hnswlib
+# pip install nano-graphrag
+# pip install git+https://github.com/HKUDS/LightRAG.git
+# pip install umap-learn==0.5.5
+# pip install graspologic==3.3.0
+# pip uninstall -y hnswlib chroma-hnswlib && pip install chroma-hnswlib
 
+# 安装依赖 方式2
+# pip install -r requirements.txt
 
 # 严格模式: 脚本错误立即退出, 未定义变量报错
 set -euo pipefail
