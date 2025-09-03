dde176ce60b91530cca8b22502f6ed64937de791
huyanwei <hywchina@126.com>
2025-09-03 14:24:21 +0800

    docker 部署相关

--- services_docker/start_serve_docker.sh
+++ services_docker/start_serve_docker.sh
@@ -1,19 +1,16 @@
-# 构建镜像
-docker build --target lite -t my-app-lite -f Dockerfile.ai-diagnosis .
-docker build --target full -t chifeng-ai-diagnosis:v0.1 -f Dockerfile.ai-diagnosis .
-docker build --target ollama -t my-app-ollama -f Dockerfile.ai-diagnosis .
+# 构建镜像 原始的 dockerfile（三个版本，lite、full、ollama）
+docker build --target lite -t my-app-lite -f Dockerfile .
+docker build --target full -t chifeng-ai-diagnosis:v0.1 -f Dockerfile .
+docker build --target ollama -t my-app-ollama -f Dockerfile .
 
+# 新的 单独 dockerfile
+docker build -t chifeng-ai-diagnosis:v0.1 -f Dockerfile.ai-diagnosis .
 
 # 创建容器
-docker run -it \
+docker run -itd \
 -e GRADIO_SERVER_NAME=0.0.0.0 \
 -e GRADIO_SERVER_PORT=7860 \
 -v ./ktem_app_data:/app/ktem_app_data \
--v ./logs:/app/logs \
+-v ./logs-1:/app/logs \
 -p 7860:7860  \
-chifeng-ai-diagnosis:v0.1 /bin/bash
-
-
-
-# 进入容器
-docker exec -it $(docker ps -q --filter ancestor=ghcr.io/cinnamon/k
+chifeng-ai-diagnosis:v0.1 bash 
