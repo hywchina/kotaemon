2097c0022fad1b6b1495b1189cc4f7f5892e0541
huyanwei <hywchina@126.com>
2025-11-24 20:53:11 +0800

    update

--- services_docker/start_serve_docker.sh
+++ services_docker/start_serve_docker.sh
@@ -10,10 +10,12 @@ docker build -t chifeng-ai-diagnosis:v0.1 -f Dockerfile.ai-diagnosis .
 docker run -itd \
 -e GRADIO_SERVER_NAME=0.0.0.0 \
 -e GRADIO_SERVER_PORT=7860 \
--v ./ktem_app_data-1:/app/ktem_app_data \
--v ./logs-1:/app/logs \
+-v ./ktem_app_data:/app/ktem_app_data \
+-v ./logs:/app/logs \
+-v ./models:/app/models \
 -p 7860:7860  \
-chifeng-ai-diagnosis:v0.1 bash 
+--name ai-diagnosis-service \
+chifeng-ai-diagnosis:v0.1 
 
 # 容器--》镜像--》离线压缩包--》加载离线压缩包
 docker commit <容器ID或容器名> <新镜像名>:<tag>
