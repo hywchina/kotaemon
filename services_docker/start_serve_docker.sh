3c6c3e01ef07699760c9206c9e68d7050a271758
huyanwei <hywchina@126.com>
2025-11-26 07:35:26 +0800

    update

--- services_docker/start_serve_docker.sh
+++ services_docker/start_serve_docker.sh
@@ -4,6 +4,7 @@ docker build --target full -t chifeng-ai-diagnosis:v0.1 -f Dockerfile .
 docker build --target ollama -t my-app-ollama -f Dockerfile .
 
 # 新的 单独 dockerfile
+rm -rf ktem_app_data logs venv # 删除旧数据
 docker build -t chifeng-ai-diagnosis:v1.0.0 -f Dockerfile.ai-diagnosis.v1.0.0 .
 
 # 创建容器
